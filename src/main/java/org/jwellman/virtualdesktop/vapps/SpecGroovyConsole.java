ae61a34bac228c4a9727bede01ece4a2fb035bd0
kansasSamurai <kansasSamurai@users.noreply.github.com>
2019-12-06 17:58:31 -0600

    much improved... supports internal/external frame

--- src/main/java/org/jwellman/virtualdesktop/vapps/SpecGroovyConsole.java
+++ src/main/java/org/jwellman/virtualdesktop/vapps/SpecGroovyConsole.java
@@ -1,36 +1,103 @@
 package org.jwellman.virtualdesktop.vapps;
 
 import groovy.ui.Console;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 import java.util.HashMap;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 /**
- *
+ * A vapp specification for the Groovy Console
+ * TODO 12/5/2019 :: this works but maybe I should make this a "groovy class"!?
+ * TODO 12/6/2019 :: the only bug I see at the moment is... if I close the original external JFrame
+ *                   then the "capture standout output/error" feature no longer works.
+ * TODO 12/6/2019 :: another bug I just found because I did not know it was a feature is... drag/drop
+ *                   supposedly you can drop a .groovy file from the file browser to open it
+ * 
  * @author rwellman
  */
 public class SpecGroovyConsole extends VirtualAppSpec {
     
     public SpecGroovyConsole() {
         super();
-        this.setTitle("Groovy Console");
-        this.setContent(new JPanel());
-
-        Console c = new Console();
-        c.run();
+        
+        // the framework handles the title in both scenarios regarding 'internalFrameProvider'
+        this.setTitle("Groovy Console"); 
+        
+        this.internalFrameProvider = true;
+        if (this.internalFrameProvider) {
+        	// do nothing here... it will be done in populateInternalFrame()
+        } else {
+        	// populateInternalFrame() will NOT be called so do "stuff" here...
+        	// This works but results in an external JFrame...
+            this.setContent(new JPanel());
+            
+	        Console c = new Console();
+	        c.run();        	
+        }
     }
     
     @Override
     public void populateInternalFrame(JInternalFrame frame) {
-        final HashMap map = new HashMap();
-        map.put("rootContainerDelegate", frame);
-        
-        Console c = new Console();
-        c.run(map); // this is a hack! not sure if it will even work
-        
-        // This works sorta but trying something else
-        // this.setContent(this.createDefaultContent(c.getFrame().getContentPane()));        
+    	System.out.println("populateInternalFrame()");
 
+    	boolean trySomethingElse = false;
+    	if (trySomethingElse) {
+    		// 12/5/2019... I thought this worked once...
+    		// maybe I thought it would but never finished the POC...
+            final HashMap<String, Component> map = new HashMap<>();
+            map.put("rootContainerDelegate", frame);
+            
+            Console c = new Console();
+            c.run(map); // this is a hack! not sure if it will even work
+    		
+    	} else {
+
+    		// This works ... 
+	        Console c = new Console(); c.run();        	
+	        final JFrame jframe = (JFrame) c.getFrame();	        
+            this.setContent(this.createDefaultContent(jframe.getContentPane()));
+            
+    		frame.add(this.getContent());
+      		frame.setJMenuBar(jframe.getRootPane().getJMenuBar());      		
+    		// DONE apparently the groovy .png is "big"... resize to look better in the DesktopManager
+    		frame.setFrameIcon(new ImageIcon( jframe.getIconImage().getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH) ));
+    		
+    		SwingUtilities.invokeLater(() -> {
+    			// TODO this is required because the edit panel and output panel do not have preferred sizes
+    			frame.setPreferredSize(new Dimension(200, 300)); 
+    			
+        		frame.pack();
+        		frame.setVisible(true);    			
+    		} );
+    		
+    	}
+
+        
+    }
+    
+    // I'm not using this at the moment but it was found as an alternative to the resizing
+    // code above; not sure of the pros/cons of either approach.  FWIW, The approach used
+    // above is a lot less code.
+    private Image getScaledImage(Image srcImg, int w, int h){
+        BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = resizedImg.createGraphics();
+
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(srcImg, 0, 0, w, h, null);
+        g2.dispose();
+
+        return resizedImg;
     }
     
 }

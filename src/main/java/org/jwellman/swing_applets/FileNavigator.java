a3f0e336f812230b56ed12aff7651d65ce82ba40
kansasSamurai <kansasSamurai@users.noreply.github.com>
2020-01-06 17:25:51 -0600

    Add SwingViewCreator feature

--- src/main/java/org/jwellman/swing_applets/FileNavigator.java
+++ src/main/java/org/jwellman/swing_applets/FileNavigator.java
@@ -2,6 +2,9 @@ package org.jwellman.swing_applets;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -15,6 +18,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTree;
+import javax.swing.SwingConstants;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 
@@ -31,9 +35,10 @@ public class FileNavigator extends JPanel {
     private JLabel status;
     private JTextArea preview;
     private JComponent viewer;
+    private SwingViewCreator imageView;
 	private final Dimension dimFilePreview = new Dimension(149,149);
 	private final String[] imageTypes = ".png;.jpg;.gif;.ico".split(";");
-
+	private final ImageScaler SCALER = new ImageScaler();
 
     /**
      * Create a FileNavigator whose root is the directory specified.
@@ -50,7 +55,7 @@ public class FileNavigator extends JPanel {
     }
     
     public FileNavigator(String directory, JComponent view, TreeSelectionListener listener) {
-    	super(new BorderLayout());
+    	super(new BorderLayout());    	
     	
         status = new JLabel(directory);
 
@@ -67,7 +72,7 @@ public class FileNavigator extends JPanel {
     		break;
     	case 3:
             if (view == null) view = new OverflowX(); 
-            if (listener == null) listener = new ImagesPanelTreeSelectionListener((JPanel) view);
+            if (listener == null) listener = new ImagesPanelTreeSelectionListener((JPanel) view, this);
     		break;    		
     	}
         viewer = view;
@@ -78,7 +83,7 @@ public class FileNavigator extends JPanel {
             // preview.setWrapStyleWord(false);        	
         }
     	
-        tree = new JTree(new FileSelectorModel(directory));
+        tree = new JTree(new FileSelectorModel(directory, true));
         tree.addTreeSelectionListener(listener);
 
         this.add(status, BorderLayout.SOUTH);
@@ -91,12 +96,45 @@ public class FileNavigator extends JPanel {
     	this.status.setText(newtext);
     }
 
-    class ImagesPanelTreeSelectionListener implements TreeSelectionListener {
+    public SwingViewCreator getImageView() {
+		return imageView;
+	}
+
+	public void setImageView(SwingViewCreator imageView) {
+		this.imageView = imageView;
+	}
+
+	public ImageIcon createFileIcon(File thisfile) {
+    	ImageIcon icon = null;
+    	
+		final String lowername = thisfile.getName().toLowerCase();
+		if (lowername.endsWith(".ico")) {
+			// Java does not natively support the .ico format; use image4j
+			// http://zetcode.com/articles/javaico/
+			try {
+				icon = new ImageIcon( ICODecoder.read(thisfile).get(0) );
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		} else {
+            icon = new ImageIcon(thisfile.getAbsolutePath());        			
+		}
+		
+		return icon;
+	}
+	
+	public BufferedImage createScaledImage(ImageIcon icon, int w, int h) {
+    	return SCALER.scaleImage( icon, BufferedImage.TYPE_INT_RGB, w, h);			
+	}		
+	
+	class ImagesPanelTreeSelectionListener implements TreeSelectionListener {
 
     	JPanel panelasview;
+    	FileNavigator navigator;
     	
-    	public ImagesPanelTreeSelectionListener(JPanel view) {
+    	public ImagesPanelTreeSelectionListener(JPanel view, FileNavigator fnav) {
     		panelasview = view;
+    		navigator = fnav;
     	}
     	
 		@Override
@@ -114,16 +152,50 @@ public class FileNavigator extends JPanel {
             	
 	                if (selectedNode.isDirectory()) {
 	                	JButton button = null; // reusable objref
+            			JComponent view = null;
 	                	
 	                	final File[] files = selectedNode.listFiles();
 	                	for (File thisfile : files) {
 	                		if (this.isImageFile(thisfile)) {
-	    		                // Update the file preview window...
-	    	                	button = new JButton(thisfile.getName());
-	    	                	button.setMinimumSize(dimFilePreview);
-	    	                	button.setMaximumSize(dimFilePreview);
-	    	                	button.setPreferredSize(dimFilePreview);
-	    	                	panelasview.add(button);	                			
+	                			if (imageView != null) {
+	                				view = imageView.createView(thisfile, navigator);
+	                			} else {
+		    		                // Update the file preview window...
+		    	                	button = new JButton(thisfile.getName());
+		    	                	button.setMinimumSize(dimFilePreview);
+		    	                	button.setMaximumSize(dimFilePreview);
+		    	                	button.setPreferredSize(dimFilePreview);
+		    	                	// button.setVerticalAlignment(SwingConstants.BOTTOM);
+		    	                	button.setVerticalTextPosition(SwingConstants.BOTTOM);
+		    	                	button.setHorizontalTextPosition(SwingConstants.CENTER);
+
+		    	                    if (true) {
+		    	                    	ImageIcon icon = null;
+		    	                    	
+		    	                		final String lowername = thisfile.getName().toLowerCase();
+		    	                		if (lowername.endsWith(".ico")) {
+		    	                			// Java does not natively support the .ico format; use image4j
+		    	                			// http://zetcode.com/articles/javaico/
+		    	                			try {
+		    	    							icon = new ImageIcon( ICODecoder.read(thisfile).get(0) );
+		    	    						} catch (IOException e1) {
+		    	    							e1.printStackTrace();
+		    	    						}
+		    	                		} else {
+		    	                            icon = new ImageIcon(thisfile.getAbsolutePath());        			
+		    	                		}
+		    	                		
+		    	                        if (icon != null) {
+		    	                        	// TODO consider using https://github.com/rkalla/imgscalr/ instead
+		    	                        	BufferedImage image = SCALER.scaleImage( icon, BufferedImage.TYPE_INT_RGB, 100, 100);
+		    	                        	button.setIcon(new ImageIcon(image));	    	                        
+		    	                        }
+		    	                    }            	
+
+		    	                	view = button; // panelasview.add(button);
+	                			}
+	    	                	panelasview.add(view);
+	    	                	
 	                		}	                		
 	                	}
 	                } else {
@@ -223,5 +295,63 @@ public class FileNavigator extends JPanel {
     	
     }
     
-}
+    class ImageScaler {
 
+    	/**
+         * Scale an image while preserving aspect ratio
+         *
+         * @param args Name of image file to be scaled (testing only)
+         *
+         */
+        public void main(String[] args) throws Exception {
+	    	BufferedImage in = javax.imageio.ImageIO.read(new java.io.File(args[0]));
+	    	BufferedImage out = new ImageScaler().scaleImage(in, BufferedImage.TYPE_INT_RGB, 100, 200);
+	    	javax.imageio.ImageIO.write(out, "JPG", new java.io.File("scaled.jpg"));
+        }
+
+        public BufferedImage scaleImage(ImageIcon icon, int typeIntRgb, int w, int h) {
+            final BufferedImage resizedImg = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+            
+            final Graphics2D g2 = resizedImg.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g2.drawImage(icon.getImage(), 0, 0, icon.getIconWidth(), icon.getIconHeight(), null);
+            g2.dispose();
+
+            return scaleImage(resizedImg, typeIntRgb, w, h);
+		}
+
+		/**
+         *
+         * @param image The image to be scaled
+         * @param imageType Target image type, e.g. TYPE_INT_RGB
+         * @param newWidth The required width
+         * @param newHeight The required width
+         *
+         * @return The scaled image
+         */
+        public BufferedImage scaleImage(BufferedImage image, int imageType, int newWidth, int newHeight) {
+
+	    	// Make sure the aspect ratio is maintained, so the image is not distorted
+	    	double thumbRatio = (double) newWidth / (double) newHeight;
+	    	int imageWidth = image.getWidth(null);
+	    	int imageHeight = image.getHeight(null);
+	    	double aspectRatio = (double) imageWidth / (double) imageHeight;
+	
+	    	if (thumbRatio < aspectRatio) {
+	    	    newHeight = (int) (newWidth / aspectRatio);
+	    	} else {
+	    	    newWidth = (int) (newHeight * aspectRatio);
+	    	}
+	
+	    	// Draw the scaled image
+	    	BufferedImage newImage = new BufferedImage(newWidth, newHeight, imageType);
+	    	Graphics2D graphics2D = newImage.createGraphics();
+	    	graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+	    	graphics2D.drawImage(image, 0, 0, newWidth, newHeight, null);
+	
+	    	return newImage;
+        }
+        
+    }
+    
+}
